f6189d83a397b17d4288a3f93de61522ded314ce
Lazar Prijovic @madrazzl3 <madrazzl3@gmail.com>
2021-06-25 18:46:44 +0300

    Add some more docstrings.

--- pybitcoin/scriptsig.py
+++ pybitcoin/scriptsig.py
@@ -2,6 +2,18 @@ from pydantic import BaseModel
 
 
 class ScriptSig(BaseModel):
-    """A ScriptSig."""
+    """Represents ScriptSig.
+    
+    A ScriptSig is a part of transaction's input, and is the first half of a script.
+
+    Args:
+        asm (str): The assembly representation of the script.
+        hex (str): The hex representation of the script.
+
+    Note:
+        Learn more about `transaction structure`__.
+
+    .. __: https://en.bitcoin.it/wiki/Transaction#Input
+    """
     asm: str
     hex: str
--- pybitcoin/vin.py
+++ pybitcoin/vin.py
@@ -4,7 +4,20 @@ from .scriptsig import ScriptSig
 
 
 class VIn(BaseModel):
-    """A VIn."""
+    """Represents transaction's input.
+    
+    Args:
+        coinbase (str, optional): The scriptSig if this was a coinbase transaction.
+        txid (str, optional): The transaction's ID.
+        vout (int, optional): The index of the output.
+        script_sig (ScriptSig, optional): The ScriptSig.
+        sequence (int): The transaction's sequence number.
+
+    Note:
+        Learn more about `transaction input structure`__.
+
+    .. __: https://en.bitcoin.it/wiki/Transaction#Input
+    """
     coinbase: Optional[str]
     txid: Optional[str]
     vout: Optional[conint(ge=0)]
